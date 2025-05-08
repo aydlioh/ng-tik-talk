1c0e7f0db08048b89c19e206c2604affa5c460a8
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2025-05-08 22:01:36 +0500

    feat

---
+++ src/app/features/sidebar/subscriber-card/subscriber-card.component.ts
@@ -0,0 +1,12 @@
+import { Profile } from '@/shared/models/dto/profile.dto';
+import { Component, Input } from '@angular/core';
+import { SharedModule } from "../../../shared/shared.module";
+
+@Component({
+  selector: 'app-subscriber-card',
+  imports: [SharedModule],
+  templateUrl: './subscriber-card.component.html',
+})
+export class SubscriberCardComponent {
+  @Input() profile!: Profile;
+}
